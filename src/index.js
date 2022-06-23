2e4214739af23bdfeb0cfbecf4f64f0576864f41
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-24 05:41:57 +0600

    Hello world.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Counter from './components/counter';
 import reportWebVitals from './reportWebVitals';
 
 import 'bootstrap/dist/css/bootstrap.css'
@@ -9,7 +10,7 @@ import 'bootstrap/dist/css/bootstrap.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>
 );
 
