295b8054d009280f22b601b6cdfb084ea34ef74f
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-25 08:05:04 +0600

    Add font-awesome.

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,11 +10,19 @@ class Counter extends Component {
         this.setState({ count: this.state.count + 1})
     }
 
+    handleDecrement= (product) => {
+        console.log(product)
+        this.setState({ count: this.state.count - 1})
+    }
+
     render() {
         return (
             <React.Fragment>
-                <span className="badge bg-primary m-2">{this.state.count}</span>
-                <button onClick={() => this.handleIncrement({ id: 1})} className="btn btn-secondary btn-sm">Increment</button>
+                <div className="m-2">
+                    <button onClick={() => this.handleDecrement({ id: 1})} className="btn btn-secondary btn-sm"><i className="fa-solid fa-circle-minus"></i></button>
+                        <span className="badge bg-primary m-2">{this.state.count}</span>
+                    <button onClick={() => this.handleIncrement({ id: 1})} className="btn btn-secondary btn-sm"><i className="fa-solid fa-circle-plus"></i></button>
+                </div>
             </React.Fragment>
         )
     }
