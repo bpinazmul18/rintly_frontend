d828841bc4b8fe9f0ec3e37b490dbee7875aa804
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-28 08:09:10 +0600

    Remove class component insted functional component.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,9 @@
-import React, { Component } from 'react';
-
-class Navbar extends Component {
-    render() { 
-        return (
-            <nav className="navbar navbar-light bg-light">
-                <div className="container-fluid">
-                    <a className="navbar-brand" href="#">Navbar <span className='badge bg-secondary rounded-circle'>{this.props.totalCounter}</span></a>
-                </div>
-            </nav>
-        );
-    }
-}
+const Navbar = ({ totalCounter }) => (
+    <nav className="navbar navbar-light bg-light">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="#">Navbar <span className='badge bg-secondary rounded-circle'>{totalCounter}</span></a>
+        </div>
+    </nav>
+);
  
 export default Navbar;
