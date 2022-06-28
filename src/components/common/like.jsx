1996b9fc79dc60c02d8759bc5b8fb81925a4bba9
Nazmul Haque <bpinazmul18@gmail.com>
2022-06-28 20:38:17 +0600

    Fixed like rendering issue.

--- src/components/common/like.jsx
+++ src/components/common/like.jsx
@@ -1,10 +1,3 @@
-import React, { Component } from 'react';
 
-class Like extends Component {
-    render() {
-        const {liked, onLiked} = this.props
-        return <i onClick={onLiked} className={`fa-${liked ? 'solid' : 'regular'} fa-heart`} style={{ cursor: 'pointer'}}></i>
-    }
-}
- 
+const Like = ({ liked, onLiked}) => <i onClick={onLiked} className={`fa-${liked ? 'solid' : 'regular'} fa-heart`} style={{ cursor: 'pointer'}}></i>
 export default Like;
