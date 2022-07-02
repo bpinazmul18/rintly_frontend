13bd63945f19e02c66afa948d348800ac41efed1
Nazmul haque <bpinazmul18@gmail.com>
2022-07-02 15:24:17 +0600

    Practice spreed operator.

--- src/index.js
+++ src/index.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import {compose, pipe} from 'lodash/fp'
-
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
 
-// const numbers = [1, 2, 3, 4]
-// const result = numbers.map(n => n * 2)
-// console.log(result)
+const person = {
+    name: 'Nazmul',
+    address: {
+        country: 'usa',
+        city: 'sanfransisco'
+    }
+}
+console.log(person)
+person.name = 'test'
+console.log(person)
 
-let input = '   JavaScript  '
-let output = '<div>' + input.trim() + '</div>'
+const updated = Object.assign({}, person, {name: 'test2', age: 30})
+console.log(updated)
 
-const trim = str => str.trim()
-const wrap = type => str => `<${type}>${str}</${type}>`
-const toLowerCase = str => str.toLowerCase()
-
-const transform = pipe(trim, toLowerCase, wrap('div'))
-console.log(transform(input))
-
-
+const spreedOp = {...person, address: {...person.address, city: 'new york'}, name: 'test3', age: 40}
+console.log(spreedOp)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
