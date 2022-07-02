72d35d7c61db5b5c8c710c015b7d5b3d41c828ae
Nazmul Haque <bpinazmul18@gmail.com>
2022-07-02 06:56:29 +0600

    Use react-router-dom Link.

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => (
     <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
+        <div className="container">
             <a className="navbar-brand" href="#!">Navbar</a>
+
+            <ul className="navbar-nav flex-row">
+                <li className="nav-item">
+                    <Link className="nav-link px-3" to="/">Home</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link px-3" to="/about">About</Link>
+                </li>
+                <li className="nav-item">
+                    <Link className="nav-link px-3" to="#!">Pricing</Link>
+                </li>
+            </ul>
         </div>
     </nav>
 );
