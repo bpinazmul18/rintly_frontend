0bef89036612810b41caea6a61f07109718392c3
Nazmul haque <bpinazmul18@gmail.com>
2022-07-02 16:35:08 +0600

    Immutable.js practice.

--- src/index.js
+++ src/index.js
@@ -5,28 +5,19 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
 import './index.css';
+import {Map} from "immutable";
 
 
-const numbers = [1, 2, 3]
+let book = Map({ title: 'title 1'})
 
-// adding start
-const addedStart = [4, ...numbers]
-console.log(addedStart)
-// adding end
-const addedEnd = [...numbers, 4]
-console.log(addedEnd)
+function publish(book) {
+    return book.set('isPublish', true)
+}
 
-// findIndex
-const index = numbers.indexOf(21)
-console.log(index)
+book = publish(book)
 
-// remove item
-const remove = numbers.filter(n => n !== 2)
-console.log(remove)
-
-// update item
-const update = numbers.map(n => n === 2 ? 20 : n)
-console.log(update)
+console.log(book.toJS())
+console.log(book.get('title'))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
