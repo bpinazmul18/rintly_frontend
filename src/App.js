900c9df1a538588c471b9d082bc48607f0f7df43
Nazmul Haque <bpinazmul18@gmail.com>
2022-07-02 06:39:00 +0600

    Defined Routes, Route and element.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from './components/navbar';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import Movies from './components/movies';
+import { Route, Routes } from 'react-router-dom';
 
 class App extends Component {
   render () {
@@ -14,6 +15,13 @@ class App extends Component {
         <Navbar />
         <main className='container'>
           <Movies/>
+
+          <div className='content'>
+            <Routes>
+              <Route path="/" element={<h1>Hello home</h1>} />
+              <Route path="about" element={<h1>Hello about</h1>} />
+            </Routes>
+          </div>
           <ToastContainer
               position="top-right"
               autoClose={5000}
