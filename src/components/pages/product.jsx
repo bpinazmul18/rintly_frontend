03cccde215912a051e94eadd7dbba0bbd36b87b7
Nazmul Haque <bpinazmul18@gmail.com>
2022-07-04 22:23:08 +0600

    Navigate programmaticaly.

--- src/components/pages/product.jsx
+++ src/components/pages/product.jsx
@@ -1,16 +1,22 @@
-import { useParams, useLocation } from "react-router-dom"
+import { useParams, useLocation, useNavigate } from "react-router-dom"
 import queryString from "query-string"
 
 // eslint-disable-next-line
 export default function (props) {
     const result = useParams()
     const location = useLocation()
+    const navigate = useNavigate()
     const x = queryString.parseUrl(location.search)
     console.log(x)
+
+    const handleRoute = () => {
+        navigate('/')
+    }
 
     return (
         <div>
             product <span>{'1'}</span>
+            <button onClick={() => handleRoute()}>Goto home page</button>
         </div>
     )
 }
