8984488d653542fbe1f741a0cd0f34fc98d637e1
Nazmul haque <bpinazmul18@gmail.com>
2022-07-05 17:02:36 +0600

    Working on reduxt-devtools-extensions.

--- src/store.js
+++ src/store.js
@@ -1,9 +1,10 @@
 import {createStore} from 'redux'
+import {devToolsEnhancer} from 'redux-devtools-extension'
 import reducer from './reducer'
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    devToolsEnhancer({ trace: true })
 )
 
 export default store
