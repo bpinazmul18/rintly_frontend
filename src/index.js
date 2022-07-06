afca3db6aada7806f5395a7fbbe345d12b526d4d
Nazmul haque <bpinazmul18@gmail.com>
2022-07-06 16:50:11 +0600

    Combine reducers.

--- src/index.js
+++ src/index.js
@@ -12,18 +12,25 @@ import { projectAdded } from "./store/projects";
 
 const store = configureStore()
 
-store.subscribe(() => {
-    console.log('Store changed!', store.getState())
-})
+// store.subscribe(() => {
+//     console.log('Store changed!', store.getState())
+// })
 
 store.dispatch(bugAdded({ description: 'Bug add 1'}))
+store.dispatch(bugAdded({ description: 'Bug add 2'}))
+store.dispatch(bugAdded({ description: 'Bug add 3'}))
 store.dispatch(projectAdded({ name: 'Project add 1'}))
+store.dispatch(projectAdded({ name: 'Project add 2'}))
+store.dispatch(projectAdded({ name: 'Project add 3'}))
 store.dispatch(bugResolved({id: 1}))
 store.dispatch(bugRemoved({id: 1}))
 
-console.log(store.getState())
+const x = store.getState().entities.bugs.filter(bug => !bug.resolved)
+console.log(x)
+const y = store.getState().entities.bugs.filter(bug => !bug.resolved)
+console.log(y)
 
-
+console.log(x === y)
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <App/>
