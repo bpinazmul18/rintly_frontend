ca07fafb06ab9af049a9f7fdcd7e887ee8c9587c
Nazmul haque <bpinazmul18@gmail.com>
2022-07-06 17:48:42 +0600

    Add users slice.

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ import './index.css';
 import configureStore from './store/configureStore'
 import {bugAdded, bugRemoved, bugResolved, getUnResolvedBugs} from "./store/bugs";
 import { projectAdded } from "./store/projects";
+import {userAdded} from "./store/users";
 
 const store = configureStore()
 
@@ -18,6 +19,10 @@ const store = configureStore()
 
 store.dispatch(bugAdded({ description: 'Bug add 1'}))
 store.dispatch(bugAdded({ description: 'Bug add 2'}))
+
+store.dispatch(userAdded({name: 'Nazmul Haque'}))
+store.dispatch(userAdded({name: 'Borsha'}))
+
 store.dispatch(bugAdded({ description: 'Bug add 3'}))
 store.dispatch(projectAdded({ name: 'Project add 1'}))
 store.dispatch(projectAdded({ name: 'Project add 2'}))
