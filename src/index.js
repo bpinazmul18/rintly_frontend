c987301fce9b21c8eaf6b8a98f6d843523c39617
Nazmul haque <bpinazmul18@gmail.com>
2022-07-06 15:11:07 +0600

    Practicing slice in @reduxjs/toolkit.

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import './index.css';
 
 import configureStore from './store/configureStore'
 import {bugAdded, bugRemoved, bugResolved} from "./store/bugs";
+import { projectAdded } from "./store/projects";
 
 const store = configureStore()
 
@@ -16,6 +17,7 @@ store.subscribe(() => {
 })
 
 store.dispatch(bugAdded({ description: 'Bug add 1'}))
+store.dispatch(projectAdded({ name: 'Project add 1'}))
 store.dispatch(bugResolved({id: 1}))
 store.dispatch(bugRemoved({id: 1}))
 
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,7 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import reducer from './bugs'
-
-
+import reducer from './projects'
 
 export default function () {
     return configureStore({ reducer })
