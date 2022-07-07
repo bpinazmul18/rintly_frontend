63a15c06af554ef5ebc9aa0972e1b15b4708dc6a
Nazmul haque <bpinazmul18@gmail.com>
2022-07-07 12:17:41 +0600

    Dispatching function using defaultMiddleware.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,11 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from './reducer'
 import logger from "./middleware/logger";
-import func from "./middleware/func";
 
 export default function () {
     return configureStore({
         reducer,
-        middleware: [logger({ destination: 'console'}), func]
+        middleware:  (getDefaultMiddleware) => getDefaultMiddleware().concat(logger({ destination: 'console'}))
     })
 }
