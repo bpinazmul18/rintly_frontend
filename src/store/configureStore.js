1a1293e13fbc030db7ee35eda1e40804d067ca64
Nazmul haque <bpinazmul18@gmail.com>
2022-07-07 11:26:06 +0600

    Parameterizing.

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -5,6 +5,6 @@ import logger from "./middleware/logger";
 export default function () {
     return configureStore({
         reducer,
-        middleware: [logger]
+        middleware: [logger({ destination: 'console'})]
     })
 }
