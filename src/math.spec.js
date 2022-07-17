189f8a45e6efd94fe383e0ad3a31d6c1265b18eb
Nazmul haque <bpinazmul18@gmail.com>
2022-07-17 15:04:15 +0600

    First unit testing.

--- src/math.spec.js
+++ src/math.spec.js
@@ -1,1 +1,14 @@
-it('First test', () => {})
+import {isEven} from "./math";
+
+describe('isEven', () => {
+    it('should return true if given an even number.', () => {
+        const result = isEven(2)
+        expect(result).toEqual(true)
+    })
+
+    it('should return false if given an on number.', () => {
+        const result = isEven(1)
+        expect(result).toEqual(false)
+    })
+})
+
